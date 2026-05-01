68f095e876c41336f39db9a7a971a903c260df69
Eduard Goma <eduardgip85@gmail.com>
2026-05-01 12:24:58 +0200

    Added all the testing + coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/tests/setupTests.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "json"],
+      reportsDirectory: "coverage",
+      include: ["src/**/**/services/*.{ts,tsx}"],
+      exclude: [
+        "src/main.tsx",
+        "src/vite-env.d.ts",
+        "src/tests/**",
+      ],
+    },
   },
 });
